b57adee3abaf6c06d6efba8fbeeac0e28c287264
Jane <janechodance@gmail.com>
2021-09-27 13:03:58 -0400

    commit tree 16baf37dce1db29a53ae4a0e492ca65ab41e8871

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+function superbowlWin(array){
+   let result = array.find((element)=> element.result === 'W')
+    if (result) {
+    return result.year}
+   else {
+       return undefined
+   }
+   
+}
 
+
+
+// function superbowlWin(objects) {
+//    if (objects.result === "W"){
+//     return objects.year
+//    }else{
+//        return undefined
+//    }
+// }
+// let check = record.find(superbowlWin)
+// return check.year
